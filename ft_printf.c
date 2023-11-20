14837754be168e9263503324efab16e71830ef99
HKHAT <34355618+HKHAT@users.noreply.github.com>
2023-11-20 15:53:06 +0100

    ft_printf

---
+++ ft_printf.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elchakir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/30 06:17:40 by elchakir          #+#    #+#             */
+/*   Updated: 2023/10/16 15:51:09 by elchakir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	flags(va_list args, const char flag, int *len)
+{
+	char	*str;
+	void	*ptr;
+
+	if (flag == '%')
+	{
+		ft_putchar(flag, len);
+	}
+	else if (flag == 'd' || flag == 'i')
+	{
+		ft_putnbr(va_arg(args, int), len);
+	}
+	else if(flag == 'f')
+	{
+		ft_putnbr_f(va_arg(args,double),len);
+	}
+	else if (flag == 'u')
+	{
+		ft_putnbr_u(va_arg(args, unsigned int), len);
+	}
+	else if (flag == 'c')
+	{
+		ft_putchar((char)va_arg(args, int), len);
+	}
+	else if (flag == 's')
+	{
+		str = va_arg(args, char *);
+		if (str != NULL)
+		{
+			ft_putstr(str, len);
+		}
+		else
+			ft_putstr("(null)", len);
+	}
+	else if (flag == 'X' || flag == 'x')
+	{
+		ft_putnbr_base(va_arg(args, int), flag, len);
+	}
+	else if (flag == 'p')
+	{
+		ptr = va_arg(args, void *);
+		ft_putadd(ptr, len);
+	}
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		len;
+
+	va_start(args, format);
+	len = 0;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			flags(args, *format, &len);
+		}
+		else
+			ft_putchar(*format, &len);
+		format++;
+	}
+	va_end(args);
+	return (len);
+}
